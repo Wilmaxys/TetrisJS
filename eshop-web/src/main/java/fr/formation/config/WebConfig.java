57677be8c92c982957908dd041d07ca51a9fd303
Jeremy <jeremy.perrouault@ascadis.fr>
2020-09-22 12:24:22 +0200

    JOUR 3 - THYMELEAF

--- eshop-web/src/main/java/fr/formation/config/WebConfig.java
+++ eshop-web/src/main/java/fr/formation/config/WebConfig.java
@@ -3,33 +3,61 @@ package fr.formation.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.JstlView;
-import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 @Configuration
 @ComponentScan("fr.formation")
-@EnableWebMvc //ACTIVE LA DELEGUATION DE REQUETE
+@EnableWebMvc // ACTIVE LA DELEGUATION DE REQUETE
 public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		//ON DEMANDE A STRING, POUR TOUTE URL /assets/**, DE RENDRE LA RESOURCE A LA LOCALISATION /assets/
-		registry
-			.addResourceHandler("/assets/**")
-			.addResourceLocations("/assets/");
+		// ON DEMANDE A STRING, POUR TOUTE URL /assets/**, DE RENDRE LA RESOURCE A LA
+		// LOCALISATION /assets/
+		registry.addResourceHandler("/assets/**").addResourceLocations("/assets/");
+	}
+
+//	@Bean
+//	public ViewResolver viewResolver() {
+//		UrlBasedViewResolver viewResolver = new UrlBasedViewResolver();
+//		
+//		viewResolver.setPrefix("/WEB-INF/jsp/");
+//		viewResolver.setSuffix(".jsp");
+//		viewResolver.setViewClass(JstlView.class);
+//		
+//		return viewResolver;
+//	}
+
+	@Bean
+	public SpringResourceTemplateResolver templateResolver() {
+		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+
+		templateResolver.setPrefix("/WEB-INF/templates/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setCacheable(false);
+
+		return templateResolver;
 	}
 
 	@Bean
-	public ViewResolver viewResolver() {
-		UrlBasedViewResolver viewResolver = new UrlBasedViewResolver();
-		
-		viewResolver.setPrefix("/WEB-INF/jsp/");
-		viewResolver.setSuffix(".jsp");
-		viewResolver.setViewClass(JstlView.class);
-		
+	public SpringTemplateEngine templateEngine(SpringResourceTemplateResolver templateResolver) {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+
+		templateEngine.setTemplateResolver(templateResolver);
+		templateEngine.setEnableSpringELCompiler(true);
+
+		return templateEngine;
+	}
+
+	@Bean
+	public ThymeleafViewResolver viewResolver(SpringTemplateEngine templateEngine) {
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+
+		viewResolver.setTemplateEngine(templateEngine);
 		return viewResolver;
 	}
 }
